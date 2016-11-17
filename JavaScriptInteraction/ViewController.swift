65b19d00764b269b68706b0b779d7e7ddfdfd90e
Bart Fransen <hello@bartfransen.com>
2016-11-16 16:13:32 -0800

    First attempt at two way JavaScript / Swift interaction.

--- JavaScriptInteraction/ViewController.swift
+++ JavaScriptInteraction/ViewController.swift
@@ -7,19 +7,55 @@
 //
 
 import UIKit
+// YOU'LL NEED TO ADD THIS IMPORT
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKScriptMessageHandler {
 
+  // THIS IS NOW A WKWEBVIEW. UIWEBVIEW HAS BEEN DEPRECATED.
+  var webView: WKWebView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    let config = WKWebViewConfiguration()
+    
+    // THE NAME PARAMETER YOU USE HERE IS THE ONE YOU USE IN THE
+    // window.webkit.messageHandlers.scanBarcode.postMessage 
+    // CALL ON THE HTML PAGE.
+    config.userContentController.add(self, name: "scanBarcode")
+    
+    // REPLACE WITH ACTUAL URL.
+    if let path = Bundle.main.path(forResource: "index", ofType: ".html") {
+      let url = URL(fileURLWithPath: path)
+      let request = URLRequest(url: url)
+      
+      // YOU HAVE TO CREATE THE WKWEBVIEW IN CODE TO PASS THE CUSTOM CONFIGURATION.
+      // NO MORE STORYBOARD
+      self.webView = WKWebView(
+        frame: self.view.bounds,
+        configuration: config
+      )
+      self.view = self.webView!
+      
+      webView.load(request)
+    }
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+    
+    if (message.name == "scanBarcode") {
+      // IF YOU NEED IT, YOU CAN USE message.body TO PASS STUFF FROM THE HTML PAGE TO THE VC
+      print("Trigger to scan received with content: [\(message.body)]")
+      
+      // SCAN THE BARCODE
+      
+      // GENERATING CURRENT DATE TO PASS BACK TO THE HTML PAGE
+      let scannedBarcode = Date()
+      let javascript = "handleBarCodeScanReturn(\"\(scannedBarcode)\");"
+      
+      print("Executing javascript: [\(javascript)]")
+      webView.evaluateJavaScript(javascript, completionHandler: nil)
+    }
   }
-
-
 }
-
